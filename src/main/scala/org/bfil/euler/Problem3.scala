35f376801bcb047c4e1acccd77a98a90587884fb
Bruno Filippone <bruno.filippone@b-fil.com>
2013-02-21 11:30:14 +0000

    Cleaned up problems descriptions

--- src/main/scala/org/bfil/euler/Problem3.scala
+++ src/main/scala/org/bfil/euler/Problem3.scala
@@ -9,8 +9,8 @@ object Problem3 extends Problem {
   val description =
     """
     The prime factors of 13195 are 5, 7, 13 and 29.
-  	
-  	What is the largest prime factor of the number 600851475143 ?
+    
+    What is the largest prime factor of the number 600851475143 ?
     """
   val solution = 6857
   
